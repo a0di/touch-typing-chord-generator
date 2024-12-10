231b0aa510b00b09c25d5cbf1ce1017d703a6da3
Andrew <39698769+a0di@users.noreply.github.com>
2024-12-10 14:41:47 +0200

    Update pregeneration script

--- pregenerate.sh
+++ pregenerate.sh
@@ -1,120 +1,49 @@
 #!/bin/bash
 # Pregenerate chord lists
 
-# Option to build as well, to do everyting in one go
+# Option to build as well, to do everything in one go
 BUILD=$0
 
 if [[ $BUILD ]]; then
   go build src/*
 fi
 
-# QWERTY
-./chord_picker -l 0 -min 2 -max 2 -r 1 -s -d
-mv output.txt pregenerated/0-qwerty_bigrams_norepeat_shuffle_space.txt
-./chord_picker -l 0 -min 3 -max 3 -r 1 -s -d
-mv output.txt pregenerated/0-qwerty_trigrams_norepeat_shuffle_space.txt
-./chord_picker -l 0 -min 4 -max 4 -r 1 -s -d
-mv output.txt pregenerated/0-qwerty_fourgrams_norepeat_shuffle_space.txt
-./chord_picker -l 0 -min 2 -max 5 -r 1 -s -d
-mv output.txt pregenerated/0-qwerty_min2_max5_norepeat_shuffle_space.txt
+generate_chords() {
+  local layout=$1
+  local min=$2
+  local max=$3
+  local repeat=$4
+  local shuffle=$5
+  local outname=$6
+  local space=$7
 
-./chord_picker -l 0 -min 2 -max 2 -r 3 -s -d
-mv output.txt pregenerated/0-qwerty_bigrams_repeat3_shuffle_space.txt
-./chord_picker -l 0 -min 3 -max 3 -r 3 -s -d
-mv output.txt pregenerated/0-qwerty_trigrams_repeat3_shuffle_space.txt
-./chord_picker -l 0 -min 4 -max 4 -r 3 -s -d
-mv output.txt pregenerated/0-qwerty_fourgrams_repeat3_shuffle_space.txt
-./chord_picker -l 0 -min 2 -max 5 -r 3 -s -d
-mv output.txt pregenerated/0-qwerty_min2_max5_repeat3_shuffle_space.txt
+  ./chord_picker -l $layout -min $min -max $max -r $repeat -s $shuffle $space
+  echo "Generated $outname"
+  mv output.txt pregenerated/${layout}-${outname}.txt
+}
 
-./chord_picker -l 0 -min 2 -max 2 -r 1 -s
-mv output.txt pregenerated/0-qwerty_bigrams_norepeat_shuffle_nl.txt
-./chord_picker -l 0 -min 3 -max 3 -r 1 -s
-mv output.txt pregenerated/0-qwerty_trigrams_norepeat_shuffle_nl.txt
-./chord_picker -l 0 -min 4 -max 4 -r 1 -s
-mv output.txt pregenerated/0-qwerty_fourgrams_norepeat_shuffle_nl.txt
-./chord_picker -l 0 -min 2 -max 5 -r 1 -s
-mv output.txt pregenerated/0-qwerty_min2_max5_norepeat_shuffle_nl.txt
+layouts=("0-qwerty" "1-workman" "2-dvorak" "3-programmer-dvorak" "4-colemak" "5-colemak-mod-dh" "6-qwertz" "7-halmak" "8-engram" "9-maltron" "10-norman" "11-qgmlw")
+repeats=(1 3 5)
+spaces=("-d" "")
+space_suffixes=("space" "nl")
 
-./chord_picker -l 0 -min 2 -max 2 -r 3 -s
-mv output.txt pregenerated/0-qwerty_bigrams_repeat3_shuffle_nl.txt
-./chord_picker -l 0 -min 3 -max 3 -r 3 -s
-mv output.txt pregenerated/0-qwerty_trigrams_repeat3_shuffle_nl.txt
-./chord_picker -l 0 -min 4 -max 4 -r 3 -s
-mv output.txt pregenerated/0-qwerty_fourgrams_repeat3_shuffle_nl.txt
-./chord_picker -l 0 -min 2 -max 5 -r 3 -s
-mv output.txt pregenerated/0-qwerty_min2_max5_repeat3_shuffle_nl.txt
+for layout in "${layouts[@]}"; do
+  layout_num=${layout%%-*}
+  layout_name=${layout#*-}
 
-# QWERTZ
-./chord_picker -l 6 -min 2 -max 2 -r 1 -s -d
-mv output.txt pregenerated/6-qwertz_bigrams_norepeat_shuffle_space.txt
-./chord_picker -l 6 -min 3 -max 3 -r 1 -s -d
-mv output.txt pregenerated/6-qwertz_trigrams_norepeat_shuffle_space.txt
-./chord_picker -l 6 -min 4 -max 4 -r 1 -s -d
-mv output.txt pregenerated/6-qwertz_fourgrams_norepeat_shuffle_space.txt
-./chord_picker -l 6 -min 2 -max 5 -r 1 -s -d
-mv output.txt pregenerated/6-qwertz_min2_max5_norepeat_shuffle_space.txt
+  for repeat in "${repeats[@]}"; do
+    for space_index in "${!spaces[@]}"; do
+      space=${spaces[$space_index]}
+      space_suffix=${space_suffixes[$space_index]}
+      echo "Generating $layout_name repeat $repeat shuffle $space_suffix"
+      echo "Command line: ./chord_picker -l $layout_num -min 2 -max 4 -r $repeat -s 1 $space"
+      echo "Output file: ${layout_name}_${ngram}grams_repeat${repeat}_shuffle_${space_suffix}"
 
-./chord_picker -l 6 -min 2 -max 2 -r 3 -s -d
-mv output.txt pregenerated/6-qwertz_bigrams_repeat3_shuffle_space.txt
-./chord_picker -l 6 -min 3 -max 3 -r 3 -s -d
-mv output.txt pregenerated/6-qwertz_trigrams_repeat3_shuffle_space.txt
-./chord_picker -l 6 -min 4 -max 4 -r 3 -s -d
-mv output.txt pregenerated/6-qwertz_fourgrams_repeat3_shuffle_space.txt
-./chord_picker -l 6 -min 2 -max 5 -r 3 -s -d
-mv output.txt pregenerated/6-qwertz_min2_max5_repeat3_shuffle_space.txt
+      for ngram in 2 3 4; do
+        generate_chords $layout_num $ngram $ngram $repeat -s "${layout_name}_${ngram}grams_repeat${repeat}_shuffle_${space_suffix}" $space
+      done
+    done
+  done
+done
 
-./chord_picker -l 6 -min 2 -max 2 -r 1 -s
-mv output.txt pregenerated/6-qwertz_bigrams_norepeat_shuffle_nl.txt
-./chord_picker -l 6 -min 3 -max 3 -r 1 -s
-mv output.txt pregenerated/6-qwertz_trigrams_norepeat_shuffle_nl.txt
-./chord_picker -l 6 -min 4 -max 4 -r 1 -s
-mv output.txt pregenerated/6-qwertz_fourgrams_norepeat_shuffle_nl.txt
-./chord_picker -l 6 -min 2 -max 5 -r 1 -s
-mv output.txt pregenerated/6-qwertz_min2_max5_norepeat_shuffle_nl.txt
 
-./chord_picker -l 6 -min 2 -max 2 -r 3 -s
-mv output.txt pregenerated/6-qwertz_bigrams_repeat3_shuffle_nl.txt
-./chord_picker -l 6 -min 3 -max 3 -r 3 -s
-mv output.txt pregenerated/6-qwertz_trigrams_repeat3_shuffle_nl.txt
-./chord_picker -l 6 -min 4 -max 4 -r 3 -s
-mv output.txt pregenerated/6-qwertz_fourgrams_repeat3_shuffle_nl.txt
-./chord_picker -l 6 -min 2 -max 5 -r 3 -s
-mv output.txt pregenerated/6-qwertz_min2_max5_repeat3_shuffle_nl.txt
-
-# Colemak
-./chord_picker -l 4 -min 2 -max 2 -r 1 -s -d
-mv output.txt pregenerated/4-colemak_bigrams_norepeat_shuffle_space.txt
-./chord_picker -l 4 -min 3 -max 3 -r 1 -s -d
-mv output.txt pregenerated/4-colemak_trigrams_norepeat_shuffle_space.txt
-./chord_picker -l 4 -min 4 -max 4 -r 1 -s -d
-mv output.txt pregenerated/4-colemak_fourgrams_norepeat_shuffle_space.txt
-./chord_picker -l 4 -min 2 -max 5 -r 1 -s -d
-mv output.txt pregenerated/4-colemak_min2_max5_norepeat_shuffle_space.txt
-
-./chord_picker -l 4 -min 2 -max 2 -r 3 -s -d
-mv output.txt pregenerated/4-colemak_bigrams_repeat3_shuffle_space.txt
-./chord_picker -l 4 -min 3 -max 3 -r 3 -s -d
-mv output.txt pregenerated/4-colemak_trigrams_repeat3_shuffle_space.txt
-./chord_picker -l 4 -min 4 -max 4 -r 3 -s -d
-mv output.txt pregenerated/4-colemak_fourgrams_repeat3_shuffle_space.txt
-./chord_picker -l 4 -min 2 -max 5 -r 3 -s -d
-mv output.txt pregenerated/4-colemak_min2_max5_repeat3_shuffle_space.txt
-
-./chord_picker -l 4 -min 2 -max 2 -r 1 -s
-mv output.txt pregenerated/4-colemak_bigrams_norepeat_shuffle_nl.txt
-./chord_picker -l 4 -min 3 -max 3 -r 1 -s
-mv output.txt pregenerated/4-colemak_trigrams_norepeat_shuffle_nl.txt
-./chord_picker -l 4 -min 4 -max 4 -r 1 -s
-mv output.txt pregenerated/4-colemak_fourgrams_norepeat_shuffle_nl.txt
-./chord_picker -l 4 -min 2 -max 5 -r 1 -s
-mv output.txt pregenerated/4-colemak_min2_max5_norepeat_shuffle_nl.txt
-
-./chord_picker -l 4 -min 2 -max 2 -r 3 -s
-mv output.txt pregenerated/4-colemak_bigrams_repeat3_shuffle_nl.txt
-./chord_picker -l 4 -min 3 -max 3 -r 3 -s
-mv output.txt pregenerated/4-colemak_trigrams_repeat3_shuffle_nl.txt
-./chord_picker -l 4 -min 4 -max 4 -r 3 -s
-mv output.txt pregenerated/4-colemak_fourgrams_repeat3_shuffle_nl.txt
-./chord_picker -l 4 -min 2 -max 5 -r 3 -s
-mv output.txt pregenerated/4-colemak_min2_max5_repeat3_shuffle_nl.txt
